8c35a1005a90f01a513ab12434af5ed056465496
nicholasyu01 <nyu@shaw.ca>
2021-05-18 19:29:00 -0700

    added login page

--- src/index.js
+++ src/index.js
@@ -7,14 +7,47 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Admin from "layouts/Admin.js";
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
-const hist = createBrowserHistory();
+// import Register from "./components/auth/Register"
+import Login from "./components/auth/Login"
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import store from "./store";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import PrivateRoute from "./components/private-route/PrivateRoute";
 
+const hist = createBrowserHistory();
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/week" />
-    </Switch>
-  </Router>,
+  <Provider store={store}>
+    <Router history={hist}>
+      {/* <Route path="/login" component={Login} /> */}
+      <Route exact path="/" component={Login} />
+      {/* <Route path="/admin" component={Admin} /> */}
+      <Switch>
+        {/* <Redirect from="/" to="/admin/search" /> */}
+        <PrivateRoute exact path="/admin" component={Admin} />
+      </Switch>
+    </Router>
+  </Provider>
+,
   document.getElementById("root")
 );
